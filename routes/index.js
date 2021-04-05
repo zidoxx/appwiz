5a96cd9e3c90e2c8d02d10135bcf89d12d16d99f
Esteban Castaño Rivera <estebancastriver@gmail.com>
2021-04-05 18:14:30 -0500

    nuevo controlador de usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getTypeUser } = require("../lib/controllers/tipo_usuario.controller");
+const {
+    getTypeUser,
+    createTypeUser,
+} = require("../lib/controllers/tipo_usuario.controller");
+
+const {
+    getUsers,
+    createUsers,
+    deleteUserById,
+} = require("../lib/controllers/usuarios.controller");
 
 router.get("/typeusers", getTypeUser);
+router.post("/typeusers", createTypeUser);
+router.delete("/users/:codigo", deleteUserById);
+
+router.get("/users", getUsers);
+router.post("/users", createUsers);
 
 module.exports = router;
