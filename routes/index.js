1257061e4be9e57697b50f1f0e95e82d3672bd1d
Esteban Castaño Rivera <estebancastriver@gmail.com>
2021-04-05 22:32:44 -0500

    Nuevos controladores para las diferentes rutas y acceso a base de datos

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const middleware = require("../lib/middlewares/auth");
 
 const {
     getTypeUser,
@@ -12,11 +13,38 @@ const {
     deleteUserById,
 } = require("../lib/controllers/usuarios.controller");
 
+const {
+    getCategories,
+    createCategories,
+} = require("../lib/controllers/categorias.controller");
+
+const {
+    getRestaurants,
+    createRestaurants,
+} = require("../lib/controllers/restaurantes.controller");
+
+const {
+    getBills,
+    createBills,
+} = require("../lib/controllers/facturas.controller");
+
 router.get("/typeusers", getTypeUser);
+router.get("/admin", middleware, (req, res) => {
+    res.redirect("/admin.html");
+});
 router.post("/typeusers", createTypeUser);
-router.delete("/users/:codigo", deleteUserById);
 
 router.get("/users", getUsers);
 router.post("/users", createUsers);
+router.delete("/users/:codigo", deleteUserById);
+
+router.get("/categories", getCategories);
+router.post("/categories", createCategories);
+
+router.get("/restaurants", getRestaurants);
+router.post("/restaurants", createRestaurants);
+
+router.get("/bills", getBills);
+router.post("/bills", createBills);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("./routes/index"));
+
+app.post("/register", (request, response) => {
+    //Authenticate User
+    const email = request.body.email;
+    const password = request.body.password;
+    const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+    response.json({
+        accessToken,
+    });
+});
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en puerto 3000");
